91af339850acb5f5190bb1a8af8ab23d58dfb2b9
Sam Ortiz <samortiz@gmail.com>
2020-09-01 23:45:05 -0300

    Cleanup

--- src/InfoPanel.js
+++ src/InfoPanel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { utils, c, game, manage } from './functions';
+import { c, manage } from './functions';
 import './InfoPanel.css';
 
 export class InfoPanel extends React.Component {
@@ -14,19 +14,19 @@ export class InfoPanel extends React.Component {
     let planetResources = window.world.selectedPlanet.resources;
     if (world.gameState === c.GAME_STATE.MANAGE) {
       return (
-        <div class='planet-info'> 
+        <div className='planet-info'> 
           <div>Planet {world.selectedPlanet.name}</div>
           <button onClick={() => manage.buildMine()}>Build Mine</button>
-          <div class='resource-list'>Resources</div>
+          <div className='resource-list'>Resources</div>
           <div>Titatium {Math.floor(planetResources.stored.titanium)} / {Math.floor(planetResources.raw.titanium)}</div>
           <div>Gold {Math.floor(planetResources.stored.gold)} / {Math.floor(planetResources.raw.gold)}</div>
           <div>Uranium {Math.floor(planetResources.stored.uranium)} / {Math.floor(planetResources.raw.uranium)}</div>
         </div>);
         } else if (world.gameState === c.GAME_STATE.FLY) {
           return (
-          <div class='ship-info'>
+          <div className='ship-info'>
             <div>Ship</div>
-            <div class='resource-list'>Resources</div>
+            <div className='resource-list'>Resources</div>
             <div> Titatium {world.ship.cargo.titanium}</div>
             <div> Gold {world.ship.cargo.gold}</div>
             <div> Uranium {world.ship.cargo.uranium}</div>
