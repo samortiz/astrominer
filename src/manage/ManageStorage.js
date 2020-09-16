efe5930c245228582f3c5a339d0264b74d4b9924
Sam Ortiz <samortiz@gmail.com>
2020-09-16 16:56:28 -0300

    Added missing files

---
+++ src/manage/ManageStorage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ManageStorage.css';
+
+export class ManageStorage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    // let world = window.world;
+    // let planet = world.selectedPlanet;
+    // let ship = world.ship;
+    
+    return (
+      <div className='storage-section'> 
+        <div><b>Ships</b></div>
+        TODO
+      </div>);
+  }
+}
+
